66b255cc4ec700e22052c1ed285ed63d8302532d
Dmytro Polishchuk <dmytropolishchuk@Dmytros-MacBook-Air.local>
2023-10-26 23:44:14 +0300

    initial project

---
+++ Shared/build.gradle.kts
@@ -0,0 +1,52 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+}
+
+@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
+kotlin {
+    targetHierarchy.default()
+
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    
+    val xcf = XCFramework()
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "Shared"
+            xcf.add(this)
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                //put your multiplatform dependencies here
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+    }
+}
+
+android {
+    namespace = "com.nevhamovno.app"
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 24
+    }
+}
---
+++ Shared/src/commonMain/kotlin/com/nevhamovno/app/Platform.kt
@@ -0,0 +1,7 @@
+package com.nevhamovno.app
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
